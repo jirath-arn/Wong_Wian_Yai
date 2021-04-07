a472331f456767e17f44961b60a2ab51bc3ecce2
supamongkonR <73258014+supamongkonR@users.noreply.github.com>
2021-04-08 05:48:16 +0700

    Chang button

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -6,7 +6,8 @@
     <div class="container text-center position-relative" data-aos="fade-in" data-aos-delay="200">
         <h1>Find Restaurants in Thailand</h1>
         <h2>- WongWianYai -</h2>
-        <a href="#about" class="btn-get-started scrollto">{{ trans('global.login') }}</a>
+        <a href="{{route('register')}}" class="btn-get-started scrollto">register here</a>
+        <a href="{{route('login')}}" class="btn-get-started scrollto">login here</a>
     </div>
 </section><!-- End Hero -->
 @endsection
